2d0381d7b8c0dc57eb3af8ba0e606e096e9bcaf4
Jungu <wnsrn3436@gmail.com>
2018-10-25 14:39:39 +0900

    + HQ Source

---
+++ HQ/Source/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+using static System.Console;
+
+namespace HQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
